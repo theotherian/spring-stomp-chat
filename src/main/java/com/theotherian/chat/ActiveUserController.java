eac35df0196a53590193d3c6cb60cba94dbb2d44
theotherian <ian.g.simpson@gmail.com>
2014-02-20 21:04:32 -0700

    two paths working

--- src/main/java/com/theotherian/chat/ActiveUserController.java
+++ src/main/java/com/theotherian/chat/ActiveUserController.java
@@ -1,14 +1,25 @@
 package com.theotherian.chat;
 
+import javax.inject.Inject;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ActiveUserController {
   
+  private SimpMessagingTemplate template;
+
+  @Inject
+  public ActiveUserController(SimpMessagingTemplate template) {
+    this.template = template;
+  }
+  
   @MessageMapping("/activeUsers")
   public void activeUsers() {
     System.out.println("hit");
+    template.convertAndSend("/topic/active", new Greeting("wow", "such active"));
   }
 
 }
--- src/main/java/com/theotherian/chat/WebSocketConfig.java
+++ src/main/java/com/theotherian/chat/WebSocketConfig.java
@@ -16,7 +16,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/queue");
+    config.enableSimpleBroker("/queue", "/topic");
     config.setApplicationDestinationPrefixes("/app");
   }
 
